cd32dcd05b97647070045ba6ed66812f9e90c8ab
Albern79 <albern79@gmail.com>
2024-04-23 23:09:23 +0700

    add templates

--- public/index.php
+++ public/index.php
@@ -1,14 +1,27 @@
 <?php
 
+
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer('templates');
+});
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to page analazer!');
+    return $this->get('renderer')->render($response, 'index.html');
+});
+
+
+$app->get('/urls', function ($request, $response) {
+    $response->getBody()->write('sites hello to you');
     return $response;
 });
 $app->run();
